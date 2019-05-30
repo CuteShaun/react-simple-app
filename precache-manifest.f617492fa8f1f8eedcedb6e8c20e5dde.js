0274cd66e4aee5da8cdce0aa00957f6f6e13b762
CuteShaun <andrewtsapko@gmail.com>
2019-05-30 22:54:11 +0300

    Updates

---
+++ precache-manifest.f617492fa8f1f8eedcedb6e8c20e5dde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bd45535520c49b4af023",
+    "url": "/static/js/main.d7218d47.chunk.js"
+  },
+  {
+    "revision": "73ba973861789e50d243",
+    "url": "/static/js/2.a5499f1b.chunk.js"
+  },
+  {
+    "revision": "bd45535520c49b4af023",
+    "url": "/static/css/main.d1b4b777.chunk.css"
+  },
+  {
+    "revision": "f33cad95287de83ef27bda5959a5722b",
+    "url": "/index.html"
+  }
+];
